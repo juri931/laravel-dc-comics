983baadb6c8fc0db68b47fc1c5a64111ad0c9099
juri931 <impa.mat@hotmail.it>
2024-05-16 15:52:27 +0200

    [add] create file table filling

--- database/migrations/2024_05_16_133451_create_comics_table.php
+++ database/migrations/2024_05_16_133451_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('description');
+            $table->string('thumb');
+            $table->decimal('price', 9,2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
