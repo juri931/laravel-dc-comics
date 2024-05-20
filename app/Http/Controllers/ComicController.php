df301fa7c039ea58cb47a2af5186522f5e77b874
juri931 <impa.mat@hotmail.it>
2024-05-20 16:36:36 +0200

    comments

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -34,8 +34,8 @@ class ComicController extends Controller
      */
     public function store(ComicRequest $request)
     {
-
-        $request->validate([
+        // OLD request
+        // $request->validate([
             // 'title' => 'required|min:3|max:100',
             // 'description' => 'min:10|max:5000',
             // 'thumb' => 'max:5000',
@@ -43,14 +43,14 @@ class ComicController extends Controller
             // 'series' => 'min:3|max:100',
             // 'sale_date' => 'date',
             // 'type' => 'min:3|max:100',
-        ],[
+        // ],[
             // 'title.required' => 'Il titolo è obbligatorio',
             // 'title.min' => 'Il titolo deve avere almeno 3 caratteri',
             // 'title.max' => 'Il titolo deve avere al massimo 100 caratteri',
             // 'description.min' => 'La descrizione deve avere almeno 10 caratteri',
             // 'price.numeric' => 'Il prezzo deve essere un valore numerico',
             // 'sale_date.date' => 'La data deve avere un formato valido'
-        ]);
+        // ]);
 
         $form_data = $request->all();
         $form_data['artists'] = '';
